8c124d525f157385f1b3c1fb7a4ecd8ce1877851
Algirdas Jasaitis <jasaitis.algirdas22@gmail.com>
2021-08-26 18:34:02 +0300

    Added tasks back-end logic.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
 class TaskController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $list = Task::query()
+            ->where('completed', false)
+            ->where('user_id', Auth::id())
+            ->get();
+
+        return response()->json($list);
+    }
+
+    public function update($task): void
+    {
+        Task::query()
+            ->where('user_id', Auth::id())
+            ->where('id', $task)
+            ->update(['completed' => true]);
+    }
+
+    public function destroy($task): JsonResponse
+    {
+        Task::query()
+            ->where('user_id', Auth::id())
+            ->where('id', $task)
+            ->delete();
+        return response()->json();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'title' => ['required', 'string'],
+            'description' => ['required', 'string'],
+        ]);
+        Task::query()
+            ->create([
+                'user_id' => Auth::id(),
+                'title' => '',
+                'description' => '',
+            ]);
+
+        return response()->json();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::resource('tasks', \App\Http\Controllers\TaskController::class, ['only' => ['index', 'update', 'destroy', 'store']]);
 });
 
 Auth::routes();
